e3439d314786fccdb4d86352aec70d244d82a71e
kimdonghei <anytime1994@gmail.com>
2026-02-19 11:30:37 +0900

    feature: pages/hompage && pages/searchpage - 서치페이지를 홈페이지와 연결하면서 홈페이지의 메인나브생성 및 연관폴더 생성, -- 메인나브에 검색버튼은 서치페이지로 가게 설정됨 -- 레이아웃에 해당되는 부분 추가됨. -- 라우트에 서치페이지 및 코드 정리됨.

--- src/app/layouts/RootLayout.tsx
+++ src/app/layouts/RootLayout.tsx
@@ -1,13 +1,18 @@
-import { Outlet, useMatches } from 'react-router-dom';
+import { Outlet, useLocation, useMatches } from 'react-router-dom';
+
 import { TopBasicNav } from '@/widgets/top-basic-nav';
+import { TopMainNav } from '@/widgets/top-main-nav';
 
 export function RootLayout() {
   const matches = useMatches();
+  const location = useLocation();
+
   const isNotFoundPage = matches.some(match => match.id === 'not-found');
+  const isHomePage = location.pathname === '/';
 
   return (
     <div className='min-h-screen'>
-      {!isNotFoundPage && <TopBasicNav />}
+      {!isNotFoundPage && (isHomePage ? <TopMainNav /> : <TopBasicNav />)}
       <main className='mx-auto max-w-5xl px-4 py-6'>
         <Outlet />
       </main>
