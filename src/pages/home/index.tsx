929dc8805231ed07d08e2aaabf94500e7908e6e7
kimdonghei <anytime1994@gmail.com>
2026-02-19 16:46:27 +0900

    docs:index - 나중에넣어질것을 미리 작성함

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,24 @@
-export function HomePage() {
-  return <h1 className='text-2xl font-semibold'>Home</h1>;
-}
+// import { 헤더 } from "@/widgets/헤더";
+// import { 푸터 } from "@/widgets/푸터";
+// import { 그레이 아이콘 } from "@/assets/icons/그레이 아이콘";
+// import { 팔로우버튼 } from "@/shared/ui/버튼";
+// import { 텍스트카드 } from "@/widgets/text 카드";
+// import { 게시글 리스트 } from "@/widgets/post 리스트";
+
+
+export function HomePage () {
+  
+  // if (자신이 쓴 게시글도 없고, 팔로우도 없다면) {
+    {/* 그레이 아이콘이랑 검색하기버튼을 띄운다. */}
+      // <div>
+      {/* 그리고 아이콘 밑엔<h1>유저를 팔로우 해보세요!</h1> */}
+    // </div>
+    // } else {
+      {/* 게시글 리스트와 팔로우된 유저들의 텍스트 카드 */}
+    // }
+    {
+    <div>
+      <h1>홈페이지</h1>
+    </div>
+    }
+  };
