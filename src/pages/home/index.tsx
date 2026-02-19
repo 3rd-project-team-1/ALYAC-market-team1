ea595d0ad80af7c4f291eb9c8f8199f0bb5e41f2
kimdonghei <anytime1994@gmail.com>
2026-02-19 17:28:49 +0900

    refactor: page/home/index - 혹시 몰라서 리팩토링함

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,20 +5,27 @@
 // import { 텍스트카드 } from "@/widgets/text 카드";
 // import { 게시글 리스트 } from "@/widgets/post 리스트";
 
+import { TopMainNav } from "@/widgets/top-main-nav";
+
 
 export function HomePage () {
-  
-  // if (자신이 쓴 게시글도 없고, 팔로우도 없다면) {
-    {/* 그레이 아이콘이랑 검색하기버튼을 띄운다. */}
-      // <div>
-      {/* 그리고 아이콘 밑엔<h1>유저를 팔로우 해보세요!</h1> */}
-    // </div>
-    // } else {
-      {/* 게시글 리스트와 팔로우된 유저들의 텍스트 카드 */}
-    // }
-    {
+  // return (
+  //   if (isNoHavefollowers) {
+  //     <div>
+  //       <그레이 아이콘/>
+  //       <p className='text-gray-500'>팔로우하는 사용자가 없습니다.</p>  
+  //       <팔로우버튼>팔로우하기</팔로우버튼>
+  //     </div>
+  //   })
+  //   }
+
+   return (
     <div>
-      <h1>홈페이지</h1>
+      <TopMainNav />
+      <main className='mx-auto max-w-5xl'>
+        {/* <텍스트카드 />
+        <게시글 리스트 /> */}
+      </main>
     </div>
-    }
-  };
+  );
+}
