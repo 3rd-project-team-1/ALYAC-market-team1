727d253ccfbefc4a830237cda8f21e75e28a3510
YoungjaeJang7 <jyj.firstid@gmail.com>
2026-02-23 16:35:11 +0900

    feat: prettier 설정 및 변경

--- src/app/layouts/RootLayout.tsx
+++ src/app/layouts/RootLayout.tsx
@@ -2,7 +2,6 @@ import { Outlet, useLocation } from 'react-router-dom';
 
 import { TabMenu } from '@/widgets/tab-menu/ui/Tab-menu';
 
-//TODO: 세윤님 페이지 이름 변경 후 연결 예정
 const HIDE_TAB_MENU_PATHS = ['/edit-profile', '/post-create', '/signin', '/signup', '/'];
 
 export function RootLayout() {
