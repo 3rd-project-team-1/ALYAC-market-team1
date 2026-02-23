44c5f4696e20a93a5b07fd0e9cf306f1e891a22e
kimdonghei <anytime1994@gmail.com>
2026-02-23 13:59:48 +0900

    feat:404page - 서치로 넘어가는 링크 라인 추가

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -5,10 +5,9 @@ import errorLogo from '@/shared/assets/icons/full-logo-alyac-404.svg';
 export function NotFoundPage() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
-      <img src={errorLogo} alt="404 Not Found" className="w-32 mb-8" /> 
-      <h1 className="text-2xl font-bold mb-4">페이지를 찾을 수 없습니다</h1>
-      <p className="text-gray-500 mb-8">죄송합니다. 요청하신 페이지를 찾을 수 없습니다.</p>
-      <Link to="/">
+      <img src={errorLogo} alt="404 Not Found" className="w-32 mb-8" />
+      <p className="text-gray-500 mb-8">페이지를 찾을 수 없습니다. :(</p>
+      <Link to="/search">
         <Button variant="outline">뒤로 돌아가기</Button>
       </Link>
     </main>
