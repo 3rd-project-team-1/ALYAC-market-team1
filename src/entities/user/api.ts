456ad430651da9c49a982319a5516c6a60f1864d
kimdonghei <anytime1994@gmail.com>
2026-02-24 15:30:06 +0900

    docs: api - 겹칠수 있기에 호출을 좀더 개별적으로 변경

--- src/entities/user/api.ts
+++ src/entities/user/api.ts
@@ -2,14 +2,14 @@ import axiosInstance from '@/shared/api/axios';
 
 import type { Profile } from './types';
 
-export interface ProfileResponse {
+export interface GetProfileResponse {
   profile: Profile;
 }
 
 export const userApi = {
   // 프로필 조회 GET /api/profile/:accountname
   getProfile: (accountname: string) =>
-    axiosInstance.get<ProfileResponse>(`/api/profile/${accountname}`),
+    axiosInstance.get<GetProfileResponse>(`/api/profile/${accountname}`),
 
   // 팔로우 POST /api/profile/:accountname/follow
   follow: (accountname: string) => axiosInstance.post(`/api/profile/${accountname}/follow`),
