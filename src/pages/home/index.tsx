e5fe6e11801b518fa750e68912f92c85958bdcf5
junwoo <bjw9415@gmail.com>
2026-02-25 11:48:02 +0900

    refactor: 이미지파일 png 파일에서 svg-props 로 변경

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from 'react';
 
 import { Link } from 'react-router-dom';
 
-import fulllogo from '@/shared/assets/images/full-logo.png';
-import logoimg from '@/shared/assets/images/logo.png';
+import { FullLogoAlyacNoTextIcon } from '@/shared/assets/svg-props/svg-props';
+import { FullLogoAlyacPngIcon } from '@/shared/assets/svg-props/svg-props';
 import { cn } from '@/shared/lib/utils';
 import { Button } from '@/shared/ui/button';
 
@@ -15,11 +15,7 @@ const SOCIAL_PROVIDERS = ['kakao', 'google', 'facebook'] as const;
 function SplashScreen() {
   return (
     <div className="animate-in fade-in zoom-in-95 flex flex-1 flex-col items-center justify-center pb-24 duration-1000">
-      <img
-        src={fulllogo}
-        alt="알약마켓 전체 로고"
-        className="h-55 w-50 object-contain md:h-100 md:w-100"
-      />
+      <FullLogoAlyacPngIcon className="h-55 w-50 object-contain" />
     </div>
   );
 }
@@ -28,11 +24,7 @@ function SelectionScreen() {
   return (
     <>
       <div className="animate-in fade-in flex flex-1 items-center justify-center duration-700">
-        <img
-          src={logoimg}
-          alt="알약마켓 심볼 로고"
-          className="h-38.25 w-24.25 object-contain md:h-60 md:w-50"
-        />
+        <FullLogoAlyacNoTextIcon className="h-38.25 w-24.25 object-contain md:h-60 md:w-50" />
       </div>
       <div className="animate-in slide-in-from-bottom-full flex h-[45vh] w-full flex-col items-center rounded-t-[30px] bg-white px-8 pt-12 pb-16 duration-700">
         <div className="animate-in fade-in flex w-full max-w-sm flex-col space-y-4 delay-300 duration-700">
