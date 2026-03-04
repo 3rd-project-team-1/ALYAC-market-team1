0229591e7400cd539df8b5928f22f5b3909b2036
junwoo <bjw9415@gmail.com>
2026-03-04 21:49:58 +0900

    feat: zodResolver로 Zod 연결(useSignInForm)

--- src/features/auth/hooks/useSignInForm.ts
+++ src/features/auth/hooks/useSignInForm.ts
@@ -1,3 +1,4 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import axios from 'axios';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
@@ -5,10 +6,7 @@ import { toast } from 'sonner';
 
 import { useSignIn } from '@/entities/auth';
 
-export interface SignInFormData {
-  email: string;
-  password: string;
-}
+import { type SigninInput, signinSchema } from '../model/signin.schema';
 
 export const useSignInForm = () => {
   const navigate = useNavigate();
@@ -19,29 +17,32 @@ export const useSignInForm = () => {
     handleSubmit,
     setError,
     formState: { errors, isValid },
-  } = useForm<SignInFormData>({
-    defaultValues: { email: '', password: '' },
+  } = useForm<SigninInput>({
+    resolver: zodResolver(signinSchema),
+    defaultValues: {
+      user: {
+        email: '',
+        password: '',
+      },
+    },
     mode: 'onChange',
   });
 
-  const onSubmit = (data: SignInFormData) => {
-    loginMutation.mutate(
-      { user: data },
-      {
-        onSuccess: () => navigate('/feed'),
-        onError: (error) => {
-          if (axios.isAxiosError<{ message: string }>(error)) {
-            const errorMessage =
-              error.response?.data?.message || '이메일 또는 비밀번호가 일치하지 않습니다.';
-            setError('email', { message: '' });
-            setError('password', { message: errorMessage });
-          } else {
-            toast.error('알 수 없는 오류가 발생했습니다.');
-            console.error(error);
-          }
-        },
+  const onSubmit = (data: SigninInput) => {
+    loginMutation.mutate(data, {
+      onSuccess: () => navigate('/feed'),
+      onError: (error) => {
+        if (axios.isAxiosError<{ message: string }>(error)) {
+          const errorMessage =
+            error.response?.data?.message || '이메일 또는 비밀번호가 일치하지 않습니다.';
+          setError('user.email', { message: '' });
+          setError('user.password', { message: errorMessage });
+        } else {
+          toast.error('알 수 없는 오류가 발생했습니다.');
+          console.error(error);
+        }
       },
-    );
+    });
   };
   return {
     register,
