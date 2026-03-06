a3d266664a6bb99b0f0637eb5c8249802055201f
junwoo <bjw9415@gmail.com>
2026-03-06 21:14:07 +0900

    feat: user.schemas.ts 작성

--- src/entities/user/model/user.schema.ts
+++ src/entities/user/model/user.schema.ts
@@ -18,10 +18,35 @@ export const profileSchema = z.object({
   accountname: z.string(),
   intro: z.string().default(''),
   image: z.string(),
+  isfollow: z.boolean(),
+  following: z.array(z.string()),
+  follower: z.array(z.string()),
   followingCount: z.number(),
   followerCount: z.number(),
-  isfollow: z.boolean(),
 });
 
-export type UserSchema = z.infer<typeof userSchema>;
-export type ProfileSchema = z.infer<typeof profileSchema>;
+export const searchUserSchema = profileSchema.omit({ isfollow: true }).extend({
+  email: z.string(),
+});
+
+export const getProfileResponseSchema = z.object({
+  profile: profileSchema,
+});
+
+export const searchUsersResponseSchema = z.array(searchUserSchema);
+
+export const getFollowersResponseSchema = z.object({
+  follower: z.array(profileSchema),
+});
+
+export const getFollowingsResponseSchema = z.object({
+  following: z.array(profileSchema),
+});
+
+export type User = z.infer<typeof userSchema>;
+export type Profile = z.infer<typeof profileSchema>;
+export type SearchUser = z.infer<typeof searchUserSchema>;
+export type GetProfileResponse = z.infer<typeof getProfileResponseSchema>;
+export type SearchUsersResponse = z.infer<typeof searchUsersResponseSchema>;
+export type GetFollowersResponse = z.infer<typeof getFollowersResponseSchema>;
+export type GetFollowingsResponse = z.infer<typeof getFollowingsResponseSchema>;
