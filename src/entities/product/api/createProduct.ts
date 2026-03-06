a43a0e090e8cd2dbb97b5273b7250c592813466b
junwoo <bjw9415@gmail.com>
2026-03-06 10:16:10 +0900

    fix: 스키마 검증을 위해 비동기로 수정 및 응답확인

--- src/entities/product/api/createProduct.ts
+++ src/entities/product/api/createProduct.ts
@@ -2,5 +2,10 @@ import { API_ENDPOINT, axiosInstance } from '@/shared/api';
 
 import type { CreateProductInput, ProductResponse } from '../types';
 
-export const createProduct = (data: CreateProductInput) =>
-  axiosInstance.post<ProductResponse>(API_ENDPOINT.PRODUCT_CREATE, { product: data });
+export const createProduct = async (data: CreateProductInput) => {
+  const response = await axiosInstance.post<ProductResponse>(API_ENDPOINT.PRODUCT_CREATE, {
+    product: data,
+  });
+  console.log('createProduct response:', JSON.stringify(response.data, null, 2));
+  return response;
+};
--- src/entities/product/api/updateProduct.ts
+++ src/entities/product/api/updateProduct.ts
@@ -1,6 +1,9 @@
 import { API_ENDPOINT, axiosInstance } from '@/shared/api';
 
-import type { CreateProductInput, ProductResponse } from '../types';
+import type { CreateProductInput } from '../types';
 
-export const updateProduct = (id: string, data: CreateProductInput) =>
-  axiosInstance.put<ProductResponse>(API_ENDPOINT.PRODUCT_UPDATE(id), { product: data });
+export const updateProduct = async (id: string, data: CreateProductInput) => {
+  const response = await axiosInstance.put(API_ENDPOINT.PRODUCT_UPDATE(id), { product: data });
+  console.log('updateProduct response:', JSON.stringify(response.data, null, 2));
+  return response;
+};
